dd2193f29cd54e85705ec96f0a8897f2cfa0ae6f
Liju Thomas <lijuthomasliju03@gmail.com>
2025-04-24 10:56:54 +0530

    fix: added err log on env load

--- config/config.go
+++ config/config.go
@@ -19,7 +19,7 @@ type Config struct {
 func LoadConfig() Config {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file", err)
 	}
 	config := Config{
 		APIGATEWAYPORT: getEnv("APIGATEWAYPORT", "7000"),
